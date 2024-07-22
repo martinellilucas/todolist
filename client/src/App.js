ea7a644c30d12a9f60a1fae744dc4b1a5e580272
Lucas Martinelli <martinellilucas8@gmail.com>
2024-07-22 17:56:36 -0300

    Input + button

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">To Do List</h1>
+      <hr />
+      <div className="new-task">
+        <input className="input"></input>
+        <button className="button">Add</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
